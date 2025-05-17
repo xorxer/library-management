ec346620c7c53874e4f4f357d7195d0f7453c08d
Anthony Le <le672869@gmail.com>
2025-05-16 21:21:34 -0700

    Added book info content to home page, but not styled yet

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,11 +1,45 @@
-// app/(main)/page.tsx
+
+const book = {
+    title: "Automate the Boring Stuff with Python",
+    author: "Al Sweigart",
+    category: "Programming",
+    total_books: 20,
+    available_books: 20,
+    description: "If you've ever spent  hours renaming files or updating spreadsheet cells, you know how tedious tasks like these can be. But what if you could have your computer do  them for you? Automate the Boring Stuff with Python, 2nd Edition teaches even the technically uninclined how to write programs that do in  minutes what would take hours to do by hand—no prior coding experience  required!"
+}
+
+
 export default function HomePage() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold mb-4">Welcome to the Main Page</h1>
-      <p className="text-lg text-gray-600">
-        This is your home page, served at <code>/</code>!
-      </p>
-    </main>
+    <>
+      <div className="flex-col">
+        <div className="flex-row">
+          <div>{book.title}</div>
+          <div className="flex flex-row items-center gap-x-8 mb-2">
+            <div>
+              <span>By </span>
+              <span>{book.author}</span>
+            </div>
+            <div>
+              <span>Category: </span>
+              <span>{book.category}</span>
+            </div>
+          </div>
+          <div className="flex flex-row items-center gap-x-8 mb-2">
+            <div>
+              <span>Total Books: </span>
+              <span>{book.total_books}</span>
+            </div>
+            <div>
+              <span>Available Books: </span>
+              <span>{book.available_books}</span>
+            </div>
+          </div>
+          <p>{book.description}</p>
+          {/* Insert details button */}
+        </div>
+        {/* Insert book */}
+      </div>
+    </>
   );
 }
