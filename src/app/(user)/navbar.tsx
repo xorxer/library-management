ad651b08f1abb1915bc9549f89866e76c4297113
Anthony Le <le672869@gmail.com>
2025-05-24 15:54:21 -0700

    Made navbar responsive

--- src/app/(user)/navbar.tsx
+++ src/app/(user)/navbar.tsx
@@ -12,25 +12,27 @@ const user = {
 
 export default function Navbar() {
     return (
-        <nav className="flex items-center justify-between px-6 py-4 bg-home-gradient navbar">
-            <div className="flex items-center space-x-3">
+        <nav className="flex items-center justify-between px-4 py-4 bg-home-gradient navbar sm:px-6 md:px-8">
+            {/* Container on the left side has logo and app name */}
+            <div className="flex items-center space-x-2 sm:space-x-3">
                 <Link href="/" className="flex items-center gap-2 font-figtree font-bold no-underline text-white">
                     <LogoIcon
                             width={50}
                             height={50}
                             style={{color: "white"}}
                     />
-                    <span className="font-figtree font-bold text-3xl">Alcove</span>
+                    <span className="font-figtree font-bold text-2xl sm:text-3xl">Alcove</span>
                 </Link>
             </div>
-            <div className="flex flex-row space-x-5">
-                <Link href="/" className="flex items-center gap-2 font-figtree font-bold text-xl no-underline text-lightgray-100">
+            {/* Container on the right has Home with icon, Search with icon, and user profile */}
+            <div className="flex flex-row space-x-3 sm:space-x-5">
+                <Link href="/" className="flex items-center gap-2 font-figtree font-bold text-lg sm:text-xl no-underline text-lightgray-100">
                     <BiSolidHome />
-                    <span className="font-poppins font-semibold">Home</span>
+                    <span className="font-poppins font-semibold hidden sm:block">Home</span>
                 </Link>
-                <Link href="/search" className="flex items-center gap-2 font-figtree font-bold text-xl no-underline text-lightgray-100">
+                <Link href="/search" className="flex items-center gap-2 font-figtree font-bold text-lg sm:text-xl no-underline text-lightgray-100">
                     <FaSearch />
-                    <span className="font-poppins font-semibold">Search</span>
+                    <span className="font-poppins font-semibold hidden sm:block">Search</span>
                 </Link>
                 <Link href="/profile" className="flex items-center gap-2">
                     <ProfileAvatar name={user.name} imageUrl={user.image} />
